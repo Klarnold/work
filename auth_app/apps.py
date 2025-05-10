915df70b10e0993472fb1f49cfbb1832d7c81c5a
Lev <for.inst.techies@mail.ru>
2025-05-10 22:58:09 +0300

    auth_app

--- auth_app/apps.py
+++ auth_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ImageviewsConfig(AppConfig):
+class AuthAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'imageviews'
+    name = 'auth_app'
